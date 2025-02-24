151dd982c07b3cedecf6a24d6abbeaca933c31ce
FonzAye <george_super@ukr.net>
2025-02-24 22:07:55 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server is running on http://0.0.0.0:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
 // });
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
